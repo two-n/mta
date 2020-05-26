911814132af6ad546f498829d0e4f52543d75b44
auchers <aucher@two-n.com>
2020-05-25 22:05:13 -0400

    set up scroller

--- app/src/sections/App.ts
+++ app/src/sections/App.ts
@@ -1,9 +1,11 @@
 // pulls in all sections and initializes them
 // handles resize for them all
+import { selectAll } from "d3-selection"
+import * as Stickyfill from "stickyfill";
 import { Section1 } from "./Section1";
 
 import data from '../content/data.json';
-import { SECTIONS } from "../utils/constants";
+import { SECTIONS, CLASSES as C } from "../utils/constants";
 
 export class App {
   section1: Section1;
@@ -18,7 +20,25 @@ export class App {
   init() {
     const { data } = this.state
 
+    // SECTION 1
     this.section1 = new Section1({ data: data[SECTIONS.S1] })
     this.section1.init();
+
+    // polyfil for sticky positioning
+    this.setupStickyfill()
+
+    // setup resize event
+    window.addEventListener("resize", () => this.handleResize());
   }
+
+  handleResize() {
+    this.section1.handleResize()
+  }
+
+  setupStickyfill() {
+    selectAll(`${C.STICKY}`).each(function () {
+      Stickyfill.add(this);
+    });
+  }
+
 }
--- app/src/sections/Section1/index.ts
+++ app/src/sections/Section1/index.ts
@@ -1,20 +1,50 @@
 import { select } from "d3-selection";
+import "intersection-observer";
+import scrollama from "scrollama";
 import { SectionDataType } from "../../utils/types";
 import "./style.scss";
-import { CLASSES as C } from "../../utils/constants";
+import { CLASSES as C, SECTIONS as S } from "../../utils/constants";
 
 export class Section1 {
   [x: string]: any;
   data: SectionDataType;
   constructor({ data }: { data: SectionDataType }) {
     this.data = data
+    this.scroller = scrollama();
   }
 
   init() {
+    // add elements to page
+    this.setUpSection()
+
+    // force a resize on load to ensure proper dimensions are sent to scrollama
+    this.handleResize();
+
+    // setup scroller
+    this.setUpScroller()
+  }
+
+  setUpScroller() {
+    this.scroller.setup({
+      step: `#${S.S1} .${C.STEP}`,
+    })
+      .onStepEnter(this.onStepEnter)
+      .onStepExit(this.onStepExit)
+  }
+
+  onStepEnter({ element, index, direction }) {
+    console.log('step enter', element, index, direction)
+  }
+
+  onStepExit({ element, index, direction }) {
+
+  }
+
+  setUpSection() {
     const { data } = this
 
     // create section
-    this.el = select("#section1") /** unique to section */
+    this.el = select(`#${S.S1}`) /** unique to section */
       .attr("class", C.SECTION)
 
     // create graphic container
@@ -47,7 +77,21 @@ export class Section1 {
       .html(d => d.text)
   }
 
-  resize() {
+  // generic window resize listener event
+  handleResize() {
+    console.log('resizing in Section1')
+    // 1. update height of step elements
+    var stepH = Math.floor(window.innerHeight * 0.75);
+    this.steps.style("height", stepH + "px");
 
+    var figureHeight = window.innerHeight / 2;
+    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+    this.sticky
+      .style("height", figureHeight + "px")
+      .style("top", figureMarginTop + "px");
+
+    // 3. tell scrollama to update new element dimensions
+    this.scroller.resize();
   }
 }
