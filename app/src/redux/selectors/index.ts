3a0f32f7ed0026b9eaae35527d7cb763a66bc133
auchers <aucher@two-n.com>
2020-06-03 11:02:40 -0400

    create prototype section for reusable logic

--- app/src/redux/selectors/index.ts
+++ app/src/redux/selectors/index.ts
@@ -4,6 +4,7 @@ import { createSelector } from 'reselect';
 import { rollups } from 'd3-array';
 import { State } from '../../utils/types';
 import * as Helpers from '../../utils/helpers';
+import { processStations } from '../../utils/dataProcessing';
 
 /** Basic Selectors */
 export const getSectionData = (state: Store<State>) => state.getState().sectionData;
@@ -12,4 +13,4 @@ export const getTurnstileData = (state: Store<State>) => state.getState().turnst
 /** Turnstile Manipulations */
 export const getStationTimelines = createSelector([
   getTurnstileData,
-], (data) => data && rollups(data, Helpers.processStations, Helpers.getNameHash));
+], (data) => data && rollups(data, processStations, Helpers.getNameHash));
--- app/src/sections/Section1/index.ts
+++ app/src/sections/Section1/index.ts
@@ -1,14 +1,13 @@
 import { Store } from 'redux';
-import { select, Selection } from 'd3-selection';
-import 'intersection-observer';
-import scrollama from 'scrollama';
+import { Selection } from 'd3-selection';
+import Section from '../Section';
 import { SectionDataType, State } from '../../utils/types';
+import { SECTIONS as S } from '../../utils/constants';
 import './style.scss';
-import { CLASSES as C, SECTIONS as S } from '../../utils/constants';
 
 interface Props { data: SectionDataType, store: Store }
 
-export default class Section1 {
+export default class Section1 extends Section {
   store: Store<State>
 
   data: SectionDataType;
@@ -19,92 +18,15 @@ export default class Section1 {
 
   sticky:Selection;
 
+  graphic:Selection;
+
   steps: Selection;
 
   constructor({ data, store }: Props) {
-    this.store = store;
-    this.data = data;
-    this.scroller = scrollama();
+    super({ data, store, sectionName: S.S1 });
   }
 
-  init() {
-    // add elements to page
-    this.setUpSection();
+  setUpGraphic() {
 
-    // force a resize on load to ensure proper dimensions are sent to scrollama
-    this.handleResize();
-
-    // setup scroller
-    this.setUpScroller();
-  }
-
-  setUpScroller() {
-    this.scroller.setup({
-      step: `#${S.S1} .${C.STEP}`,
-    })
-      .onStepEnter(this.onStepEnter)
-      .onStepExit(this.onStepExit);
-  }
-
-  onStepEnter({ element, index, direction }) {
-    console.log('step enter', element, index, direction);
-  }
-
-  onStepExit({ element, index, direction }) {
-
-  }
-
-  setUpSection() {
-    const { data } = this;
-
-    // create section
-    this.el = select(`#${S.S1}`) /** unique to section */
-      .attr('class', C.SECTION);
-
-    // create graphic container
-    this.sticky = this.el.append('div')
-      .attr('class', C.STICKY);
-
-    // create header
-    this.sticky.append('h2').attr('class', C.SECTION_HEADER)
-      .html(data.title);
-
-    // create header
-    this.sticky.append('div')
-      .attr('class', C.GRAPHIC); /** unique to section */
-
-    // create steps
-    this.steps = this.el
-      .append('div')
-      .attr('class', C.ARTICLE)
-      .selectAll(`.${C.STEP}`).data(data.steps)
-      .join('div')
-      .attr('class', C.STEP)
-      .attr('data-step', (d) => d.step_id);
-
-    this.steps.append('h3')
-      .attr('class', C.STEP_HEADER)
-      .html((d) => d.header);
-
-    this.steps.append('div')
-      .attr('class', C.STEP_TEXT)
-      .html((d) => d.text);
-  }
-
-  // generic window resize listener event
-  handleResize() {
-    // 1. update height of step elements
-    const stepH = Math.floor(window.innerHeight * 0.75);
-    this.steps.style('height', `${stepH}px`);
-
-    const figureHeight = window.innerHeight / 2;
-    const figureMarginTop = (window.innerHeight - figureHeight) / 2;
-
-    this.sticky
-      .style('height', `${figureHeight}px`)
-      .style('top', `${figureMarginTop}px`);
-
-    // 3. tell scrollama to update new element dimensions
-    this.scroller.resize();
   }
 }
