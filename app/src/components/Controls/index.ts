cfe1a4bda28ccc2308ff27bf06162a83d4d7ea69
auchers <aucher@two-n.com>
2020-07-27 15:13:41 -0700

    add controls and stub input field

---
+++ app/src/components/Controls/index.ts
@@ -0,0 +1,58 @@
+import './style.scss';
+import { Store } from 'redux';
+import { State } from 'src/utils/types';
+import { select } from 'd3';
+import { CLASSES as C, VIEWS } from '../../utils/constants';
+import * as S from '../../redux/selectors';
+import Input from '../Input';
+import TimelineFilter from '../TimelineFilter';
+
+interface Props {
+  store: Store<State>
+}
+
+export default class Controls {
+  [x: string]: any;
+
+  constructor({ store }:Props) {
+    this.store = store;
+    this.state = store.getState();
+    this.init();
+  }
+
+  init() {
+    this.el = select('#app')
+      .append('div')
+      .attr('id', 'controls');
+
+    // TODO: pull selector for distinct list of lines
+    // TODO: write function to create swatches
+    // TODO: create array of {key: lineName, content: createSwatch(lineName)}
+    this.LineInput = new Input({
+      parent: this.el,
+      customClass: 'line',
+    });
+
+    // TODO: create selector for unique NTAs
+    // TODO: create array of {key: ntaCode, content: NTAName}
+    this.NTAInput = new Input({
+      parent: this.el,
+      customClass: 'nta',
+    });
+
+
+    const { timeline } = S.getOverallTimeline(this.state);
+    const initialWeek = S.getSelectedWeek(this.state);
+    this.timelineFilter = new TimelineFilter({
+      parent: this.el,
+      timeline,
+      initialWeek,
+    });
+  }
+
+  toggleVisibility() {
+    const state = this.store.getState();
+    const view = S.getView(state);
+    this.el.classed(C.VISIBLE, view >= VIEWS.SWARM);
+  }
+}
